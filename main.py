dbd354b7c5d0539bbd99d1c7073370a42669e10c
Isidro Gonzalez <31930997+izidro2928@users.noreply.github.com>
2021-10-06 20:19:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ movie_input_title = input("Please Type a movie title: ".upper())
 print('the input below is required'.upper())
 movie_input_year = input("Please Type the movie release date: ".upper())
 
-
 def yts_scraper(movie_title, movie_year):
     movie_title = movie_title.lower()
     movie_title = movie_title.replace(':', '')
@@ -21,28 +20,33 @@ def yts_scraper(movie_title, movie_year):
     response = requests.get(site_url)
     source = response.text
 
-    soup = BeautifulSoup(source, 'html.parser')
-    container = soup.find('div', id='movie-info')
-    container = container.find('p')
-    all_links = container.find_all('a')
+    try:
+        soup = BeautifulSoup(source, 'html.parser')
+        container = soup.find('div', id='movie-info')
+        container = container.find('p')
+        all_links = container.find_all('a')
 
-    links_path = {}
-    for index, link in enumerate(all_links):
-        link_source = link['href']
-        link_title = link.text
+        links_path = {}
+        for index, link in enumerate(all_links):
+            link_source = link['href']
+            link_title = link.text
 
-        links_path.update({
-            index: {
-                "link_source": link_source,
-                "link_title": link_title
-            }
-        })
+            links_path.update({
+                index: {
+                    "link_source": link_source,
+                    "link_title": link_title
+                }
+            })
 
-    links_path = json.dumps(links_path, indent=4)
-    return links_path
-
+        links_path = json.dumps(links_path, indent=4)
+        return links_path
+    except Exception:
+        pass
 
 results = yts_scraper(movie_input_title, movie_input_year)
 
-print("Showing Results of {}".format(movie_input_title))
-print(results)
+if results:
+    print("Showing Results of {}".format(movie_input_title))
+    print(results)
+else:
+    print("Nothing was found".upper())
